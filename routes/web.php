31995b03171e6bb8bf619729156ff8cf22b8b35c
Toc <tocwebtoc@gmail.com>
2020-06-28 11:11:14 +0200

    Basic Multi Authentication - Admin/Users

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+Route::get('admin/home', 'HomeController@adminHome')->name('admin.home')->middleware('is_admin');
