3f02028ffdb2e5f9a254206c91bd96c2d99a5a7b
Toc <tocwebtoc@gmail.com>
2020-06-29 13:34:17 +0200

    Amélioration view CRUD Users pour Admin

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -8,39 +8,41 @@
                 <div class="card-header">Liste des utilisateurs</div>
 
                 <div class="card-body">
-                    <table class="table">
-                    <thead>
-                        <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Nom</th>
-                        <th scope="col">Email</th>
-                        <th scope="col">Rôle</th>
-                        <th scope="col">Actions</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($users as $user)
+                    <div class="table-responsive">
+                        <table class="table table-striped">
+                        <thead>
                             <tr>
-                            <th scope="row">{{ $user->id }}</th>
-                            <td>{{ $user->name }}</td>
-                            <td>{{ $user->email }}</td>
-                            @if ($user->is_admin == 1)
-                            <td>Administrateur</td>
-                            @else
-                            <td>Utilisateur</td>
-                            @endif
-                            <td>
-                                <a href="{{ route('admin.users.edit', $user->id) }}"><button class="btn btn-primary">Éditer</button></a>
-                                <form action="{{ route('admin.users.destroy', $user->id) }}" method="POST" class="d-inline">
-                                        @csrf 
-                                        @method('DELETE')
-                                        <button type="submit" class="btn btn-secondary">Supprimer</button>
-                                </form>
-                            </td>
+                            <th scope="col">#</th>
+                            <th scope="col">Nom</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Rôle</th>
+                            <th scope="col">Actions</th>
                             </tr>
-                        @endforeach
-                    </tbody>
-                    </table>
+                        </thead>
+                        <tbody>
+                            @foreach ($users as $user)
+                                <tr>
+                                <th scope="row">{{ $user->id }}</th>
+                                <td>{{ $user->name }}</td>
+                                <td>{{ $user->email }}</td>
+                                @if ($user->is_admin == 1)
+                                <td>Administrateur</td>
+                                @else
+                                <td>Utilisateur</td>
+                                @endif
+                                <td class="d-flex align-items">
+                                    <a href="{{ route('admin.users.edit', $user->id) }}"><button class="btn btn-primary mr-1">Éditer</button></a>
+                                    <form action="{{ route('admin.users.destroy', $user->id) }}" method="POST" class="d-inline">
+                                            @csrf 
+                                            @method('DELETE')
+                                            <button type="submit" class="btn btn-secondary">Supprimer</button>
+                                    </form>
+                                </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
