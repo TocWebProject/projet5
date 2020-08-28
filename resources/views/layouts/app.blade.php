e994b7a799cd90c4db4a0eb71a8645bafad2f486
Toc <tocwebtoc@gmail.com>
2020-08-28 16:06:38 +0200

    SEO LAYOUT APP: Cannonical / Meta Description & Author

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,23 +1,31 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
+    <!-- Primary Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
+    <!-- Page Title -->
     <title>{{ config('app.name', 'JournalDeBord') }}</title>
 
+    <!-- Canonical & Favicon -->	
+    <link rel="canonical" href="https://tocwebproject.fr">
+    <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
+
+    <!-- Secondary Meta -->
+    <meta name="description" content="Journal de bord, l'application qui vous aides à vos organiser dans vos tâches tout en ayant 
+    accès à une citation par jour, votre météo et des images incroyables">
+    <meta name="author"      content="TocWebProject">
+    
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
     
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
-    <!-- Favicon -->
-    <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">    
